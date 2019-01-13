3fcc1bae6401784fd517b3936ad081c1d4a30d69
Fumiya Sakai <just1factory@gmail.com>
2019-01-13 20:48:54 +0900

    v0.57.8でのNativeBaseのDeprecated修正とスナップショットテスト更新

--- src/components/contents/screen/question/parts/QuestionList.js
+++ src/components/contents/screen/question/parts/QuestionList.js
@@ -11,8 +11,7 @@ import {
 } from 'react-native';
 
 import {
-  Content,
-  List
+  Content
 } from 'native-base';
 
 // よくある質問コンテンツ用の共通コンポーネント
@@ -21,16 +20,27 @@ import QuestionListItem from './QuestionListItem';
 
 export default class QuestionList extends React.Component {
 
+  // MARK: - Functions
+
+  // 取得したよくある質問コンテンツ用データを表示する処理
+  _displayQuestionList = () => {
+    const { questionItems } = this.props;
+
+    // よくある質問コンテンツ用データのリストを生成して表示する
+    return questionItems.items.map( (item, index) => {
+      return (
+        <QuestionListItem key={index} item={item} />
+      );
+    });
+  };
+
   // MARK: - Rendering Components
 
   render() {
-    const { questionItems } = this.props;
     return (
       <Content style={styles.container}>
         <QuestionListIntro />
-        <List dataArray={questionItems.items} renderRow={ (item) =>
-          <QuestionListItem item={item} />
-        } />
+        {this._displayQuestionList()}
       </Content>
     );
   };
